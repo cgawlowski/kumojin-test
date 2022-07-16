9b3ae25adc6937a46fa6bf7c3a49746a3fa2af8a
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-07-16 14:09:24 -0400

    Created routes and Events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,48 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+
+    if @event.save
+      redirect_to root_path(@event), notice: "L'évènement a bien été créé"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @event.update(event_params)
+    redirect_to event_path(@event), notice: "L'évènement a bien été mis à jour"
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to root_path(@event), notice: "L'évènement a bien été supprimé"
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :start_date, :end_date)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+end
